226c8ea53e5190b28445c34002b02dac1287c16e
ayaadel887 <aya.abdelgoad@gmail.com>
2022-11-03 16:03:32 +0200

    add context to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+// import React, { Fragment, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 //-----------myFiles--------------------
@@ -14,6 +15,7 @@ import Register from "../Pages/Register/Register";
 import Tvshowes from "./Tvshowes/Tvshowes";
 import ProtectedRout from "./protectedRoutes/protectedRout";
 import Details from "./Details/Details";
+import { MediaContextProvider } from "./mediaContext/MediaContext";
 
 //---------Css-------------------------------
 function App() {
@@ -44,20 +46,34 @@ function App() {
     <>
       <Navbar handelLogOut={handelLogOut} loginData={loginData} />
       <div className="container">
-        <Routes>
-          <Route element={<ProtectedRout loginData={loginData} />}>
-            <Route path="/" element={<Home />} />
-            <Route path="home" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="moves" element={<Moves />} />
-            <Route path="people" element={<People />} />
-            <Route path="Tvshowes" element={<Tvshowes />} />
-            <Route path="Register" element={<Register />} />
-            <Route path="details" element={<Details />} />
-          </Route>
-          <Route path="login" element={<Login getuserData={getuserData} />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+        <MediaContextProvider>
+          <Routes>
+            <Route element={<ProtectedRout loginData={loginData} />}>
+              <Route path="home" element={<Home />} />
+
+              {/*  -------------------------------------------
+ 
+              <Route path="home" element={<Home />} />
+              -------------------------------------------
+            */}
+
+              {/* <Fragment>
+              <MediaContextProvider>
+              <Route path="home" element={<Home />} />
+              </MediaContextProvider>
+            </Fragment> */}
+              <Route path="/" element={<Home />} />
+              <Route path="moves" element={<Moves />} />
+              <Route path="about" element={<About />} />
+              <Route path="people" element={<People />} />
+              <Route path="details" element={<Details />} />
+              <Route path="Tvshowes" element={<Tvshowes />} />
+              <Route path="Register" element={<Register />} />
+            </Route>
+            <Route path="login" element={<Login getuserData={getuserData} />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </MediaContextProvider>
       </div>
     </>
   );
--- src/components/mediaContext/MediaContext.js
+++ src/components/mediaContext/MediaContext.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { createContext } from "react";
 
-export let mediaContext = createContext([]);
-let mediaContextProvider = (props) => {
+export let MediaContext = createContext([]);
+
+export let MediaContextProvider = (props) => {
   let [trendingMoves, settrendingMoves] = useState([]);
   let [trendingTVShowes, settrendingTVShowes] = useState([]);
   let [trendingPeople, settrendingPeople] = useState([]);
-  let baseIMGEURL = "https://image.tmdb.org/t/p/original";
+
   let handelGetTrendingItems = async (mediatype, callbackSetFunc) => {
     let { data } = await axios.get(
       `https://api.themoviedb.org/3/trending/${mediatype}/day?api_key=a073c404739d2a76c90fb6aac68b40f6`
@@ -16,17 +17,20 @@ let mediaContextProvider = (props) => {
 
     callbackSetFunc(data.results);
   };
-  let navigate = useNavigate();
+
   useEffect(() => {
+    console.log(props, "hi from contxt");
     handelGetTrendingItems("movie", settrendingMoves);
     handelGetTrendingItems("tv", settrendingTVShowes);
     handelGetTrendingItems("person", settrendingPeople);
   }, []);
   return (
-    <mediaContext.Provider
+    <MediaContext.Provider
       value={{ trendingMoves, trendingTVShowes, trendingPeople }}
     >
       {props.children}
-    </mediaContext.Provider>
+      {console.log(props, "hi from contxt")}
+      {console.log(props.children)}
+    </MediaContext.Provider>
   );
 };
